45229852eb68ec1b30f68ff32fa4a0120ef5f278
Eriton Santos <eritonsg@gmail.com>
2021-05-27 20:06:43 -0300

    API: Add Stock domain class

---
+++ stock-manager-api/src/main/java/io/stockmanager/api/model/Stock.java
@@ -0,0 +1,15 @@
+package io.stockmanager.api.model;
+
+import java.time.LocalDate;
+
+public class Stock {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String enterpriseName;
+    private Double price;
+    private Double variation;
+    private LocalDate date;
+
+}
