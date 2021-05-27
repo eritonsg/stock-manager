e0e12ed9f84feab1e246f05851681786dfcb2b11
Eriton Santos <eritonsg@gmail.com>
2021-05-26 21:15:21 -0300

    API: Backend Application created

---
+++ stock-manager-api/src/test/java/io/stockmanager/api/StockManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stockmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
