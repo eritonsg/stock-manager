708e03c299bef97424e5deb14040faebe0cc9efd
Eriton Santos <eritonsg@gmail.com>
2021-05-27 20:13:13 -0300

    API: Add mapping to Stock domain class

--- stock-manager-api/src/main/java/io/stockmanager/api/model/Stock.java
+++ stock-manager-api/src/main/java/io/stockmanager/api/model/Stock.java
@@ -1,15 +1,40 @@
 package io.stockmanager.api.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_stocks")
 public class Stock {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "stocks_id_sequence")
+    @SequenceGenerator(name = "stocks_id_sequence", sequenceName = "stocks_table_sequence")
     private Long id;
+
+    @Column(length = 30, unique = true, nullable = false)
     private String name;
+
+    @Column(length = 255)
     private String description;
+
+    @Column(length = 80, name = "enterprise_name")
     private String enterpriseName;
+
     private Double price;
     private Double variation;
+
+    @Column(name = "registered_at")
     private LocalDate date;
 
 }
