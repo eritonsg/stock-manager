39e314c62557254e3a7b990f15631a756fa5747b
Eriton Santos <eritonsg@gmail.com>
2021-05-28 00:32:05 -0300

    API: Add an endpoint to find all stocks

--- stock-manager-api/src/main/java/io/stockmanager/api/controller/StockController.java
+++ stock-manager-api/src/main/java/io/stockmanager/api/controller/StockController.java
@@ -5,12 +5,10 @@ import io.stockmanager.api.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/stocks")
@@ -23,6 +21,11 @@ public class StockController {
     public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO stockDTO) {
         StockDTO persistedStockDTO = stockService.save(stockDTO);
         return ResponseEntity.ok(persistedStockDTO);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll() {
+        return ResponseEntity.ok(stockService.findAll());
     }
 
 }
--- stock-manager-api/src/main/java/io/stockmanager/api/service/StockService.java
+++ stock-manager-api/src/main/java/io/stockmanager/api/service/StockService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,6 +32,11 @@ public class StockService {
 
         Stock persistedStock = stockRepository.save(stockMapper.toEntity(stockDTO));
         return stockMapper.toDTO(persistedStock);
+    }
+
+    @Transactional(readOnly = true)
+    public List<StockDTO> findAll() {
+        return stockMapper.toDTO(stockRepository.findAll());
     }
 
 }
