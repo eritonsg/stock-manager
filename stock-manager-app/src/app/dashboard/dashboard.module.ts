128938e2f28122fdf0d89dd98d52d3c86779d704
Eriton Santos <eritonsg@gmail.com>
2021-05-29 12:38:14 -0300

    Api: Config routes

--- stock-manager-app/src/app/dashboard/dashboard.module.ts
+++ stock-manager-app/src/app/dashboard/dashboard.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
-
-
 @NgModule({
   declarations: [
     DashboardPageComponent,
