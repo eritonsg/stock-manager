61e4fe687a903bed56f71bb6a63a97d0d08e86b9
Eriton Santos <eritonsg@gmail.com>
2021-05-29 18:57:01 -0300

    App: Update stock card component

--- stock-manager-app/src/app/components/model/stock-model.ts
+++ stock-manager-app/src/app/components/model/stock-model.ts
@@ -1,13 +1,15 @@
-class Stock {
-  id?: number;
-  name?: string;
-  description?: string;
-  enterpriseName?: string;
-  price?: number;
+interface Stock {
+
+  id: number;
+  name: string;
+  description: string;
+  enterpriseName: string;
+  price: number;
   minimum?: number;
   exclusiveMinimum?: boolean;
-  variation?: number;
-  date?: string;
+  variation: number;
+  date: string;
+
 }
 
 export default Stock;
